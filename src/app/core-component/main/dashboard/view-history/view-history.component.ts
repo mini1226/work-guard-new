dd99bc6b7b17eb907da7772f681528d662a0c278
chiradenuwan <chiranjayadenuwan@gmail.com>
2024-11-24 18:48:36 +0530

    set view history

--- src/app/core-component/main/dashboard/view-history/view-history.component.ts
+++ src/app/core-component/main/dashboard/view-history/view-history.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {routes} from "../../../../core/helpers/routes";
 import {AthleteService} from "../../../../core/service/athlete/athlete.service";
+import {DatePipe} from "@angular/common";
+import {CommonService} from "../../../../core/service/common/common.service";
 
 @Component({
   selector: 'app-view-history',
@@ -15,7 +17,8 @@ export class ViewHistoryComponent {
 
   constructor(private route: ActivatedRoute,
               private athleteService: AthleteService,
-              private router: Router) { }
+              private router: Router, private datePipe: DatePipe, private commonService: CommonService) {
+  }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
@@ -29,31 +32,34 @@ export class ViewHistoryComponent {
     window.history.back();
   }
 
-  reset(){
+  reset() {
   }
 
 
   onSubmit(): void {
   }
 
-  performanceHistory(){
+  performanceHistory() {
     console.log(this.historyData.slice(0, 5));
-    this.router.navigate([routes.athletePerformance],{queryParams:{data:JSON.stringify(this.historyData.slice(0, 5))}});
+    this.router.navigate([routes.athletePerformance], {queryParams: {data: JSON.stringify(this.historyData.slice(0, 5))}});
   }
 
-  viewDetails(id: any) {
-    this.router.navigate([routes.viewHeartRate], {
-      queryParams: {
-        id: id
+  viewDetails(project: any) {
+    let startTime: any = this.datePipe.transform(project.start_time, 'YYYY-MM-dd HH:mm:ss:SSS');
+    this.commonService.individualRaceEndTime(startTime, project.duration).then((endTime: string) => {
+      const x = {
+        device: project.device_id,
+        startTime: startTime,
+        endTime: endTime
       }
-    });
+      this.router.navigate([routes.liveHeartRate], {queryParams: x});
+    })
   }
 
 
   private loadAllAthleteHistory() {
     this.athleteService.getAthleteHistory(this.isEditId).subscribe(value => {
-      this.historyData=value;
-      // this.sampleData
+      this.historyData = value;
     })
   }
 }
